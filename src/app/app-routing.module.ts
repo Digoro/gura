e9ee38ae0ed6b67a7c303426682475785cdff121
digoro <lsh00124@naver.com>
2019-05-19 01:40:58 +0900

    redirect survey url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 import { SurveyComponent } from './survey/survey.component';
 import { PhoneComponent } from './phone/phone.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
   { path: 'phone', component: PhoneComponent },
   { path: 'survey', component: SurveyComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', redirectTo: '/phone', pathMatch: 'full' },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SurveyComponent } from './survey/survey.component';
-import { LoginComponent } from './login/login.component';
 import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
 import { LoadingBarRouterModule } from '@ngx-loading-bar/router';
 import { LoadingBarModule } from '@ngx-loading-bar/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PhoneComponent } from './phone/phone.component';
 
-
 @NgModule({
   declarations: [
     AppComponent,
     SurveyComponent,
-    LoginComponent,
-    PhoneComponent
+    PhoneComponent,
   ],
   imports: [
     BrowserModule,
